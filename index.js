043fe70137f4410975805a628fa6dcbe6295c662
felipe barreiros <felipewww@outlook.com.br>
2021-12-16 15:01:14 -0300

    refactor agnecy controller para uma factory

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import express from 'express'
 import cors from 'cors'
-
-import CreateAgencyController from './src/controllers/CreateAgencyController.js'
 import CreateCustomerController from "./src/controllers/CreateCustomerController.js";
-import {RequestError} from "./src/utils/RequestError.js";
+import {CreateAgencyControllerFactory} from "./src/controllers/CreateAgencyController.factory.js";
 const app = express()
 
 app.use(cors())
@@ -18,58 +16,10 @@ app.use(express.json())
 // pagamento desconta o saldo
 
 app.post('/api/v1/agency', async (req, res) => {
-
-    const createAgencyCtrl = new CreateAgencyController();
-
-    let result = {
-        status: true,
-        id: null,
-        error: false,
-    }
-
-    try {
-        validateAgency(req.body);
-
-        const ctrlResult = await createAgencyCtrl.create(
-            req.body.number,
-            req.body.address,
-        );
-
-        result.id = ctrlResult[0]
-        res.statusCode = 200
-    } catch (e) {
-        if (e.name === 'RequestError') {
-            res.statusCode = 400;
-            result.error = e.message;
-        } else {
-            res.statusCode = 500;
-        }
-
-        result.status = false;
-    }
-
+    const result = await CreateAgencyControllerFactory(req);
+    res.statusCode = result.statusCode
     res.json(result)
 })
-
-function validateAgency(reqBody) {
-    let error = false;
-
-    if (reqBody.number.indexOf('?') >= 0) {
-        error = "Agency number can't have ?";
-    }
-
-    if (reqBody.number.indexOf(' ') >= 0) {
-        error = "Agency number can't blank spaces";
-    }
-
-    if (reqBody.number.length >= 8) {
-        error = "Agency number can't have more than 8 chars"
-    }
-
-    if (error) {
-        throw new RequestError(error);
-    }
-}
 
 app.post('/api/v1/customer', (req, res) => {
     const createCustomerController = new CreateCustomerController()
